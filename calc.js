ce7cde47aedafaa3366693bdbe4ead3daf9e033b
catherineedwards <catherine@edwards-au.com>
2018-10-08 07:55:20 +1300

    Got val to be parsed as a float and pushed to the answer field

--- calc.js
+++ calc.js
@@ -1,63 +1,93 @@
+window.onload = function() {
+  //add event listeners for all buttons
+  document.getElementById("1").addEventListener("click", calc);
+  document.getElementById("2").addEventListener("click", calc);
+  document.getElementById("3").addEventListener("click", calc);
+  document.getElementById("4").addEventListener("click", calc);
+  document.getElementById("5").addEventListener("click", calc);
+  document.getElementById("6").addEventListener("click", calc);
+  document.getElementById("7").addEventListener("click", calc);
+  document.getElementById("8").addEventListener("click", calc);
+  document.getElementById("9").addEventListener("click", calc);
+  document.getElementById("0").addEventListener("click", calc);
+  document.getElementById("AC").addEventListener("click", calc);
+  document.getElementById("CE").addEventListener("click", calc);
+  document.getElementById("percent").addEventListener("click", calc);
+  document.getElementById("divide").addEventListener("click", calc);
+  document.getElementById("x").addEventListener("click", calc);
+  document.getElementById("-").addEventListener("click", calc);
+  document.getElementById("=").addEventListener("click", calc);
+  document.getElementById(".").addEventListener("click", calc);
+};
 var entries = [];
 var total = 0;
 var temp = "";
 
-// add buttons functionality
-document
-  .getElementById("button")("button")
-  .on("click", function() {
-    var val = document
-      .getElementById("button")(this)
-      .text();
-    if ((val = !NaN || ".")) {
-      temp += val;
-      document.getElementById("answer").val(temp.substring(0, 10));
+function calc(event) {
+  //we will need to push all innerText to val
+  console.log(this.innerText);
+  //  document.getElementById("answer").value = this.innerText;
 
-      // add All Clear functionality
-    } else if (val === "AC") {
-      entries = [];
-      temp = "";
-      total = 0;
-      document.getElementById("answer").val("");
-    } else if (val === "CE") {
-      // add Clear Entry functionality
-      temp = "";
-      document.getElementById("answer").val("");
-    } else if (val === "x") {
-      // add multiplication operand conversion
-      entries.push(temp);
-      entries.push("*");
-      temp = [];
-    } else if (val === "÷") {
-      // add division operand conversion
-      entries.push(temp);
-      entries.push("/");
-      temp = [];
+  // add buttons functionality
+
+  var val = parseFloat(this.innerText);
+
+  if ((val.innerText = !NaN || ".")) {
+    temp += val;
+    document.getElementById("answer").value = temp;
+
+    // add All Clear functionality
+  } else if (val === "AC") {
+    entries = [];
+    temp = "";
+    total = 0;
+    document.getElementById("answer").val("");
+  } else if (val === "CE") {
+    // add Clear Entry functionality
+    temp = "";
+    document.getElementById("answer").val("");
+  } else if (val === "x") {
+    // add multiplication operand conversion
+    entries.push(temp);
+    entries.push("*");
+    temp = [];
+  } else if (val === "÷") {
+    // add division operand conversion
+    entries.push(temp);
+    entries.push("/");
+    temp = [];
+  }
+  // set the result of the calculation
+  var result = Number(entries[0]);
+  for (let i = 0; i <= entries.length; i++) {
+    var nextNum = Number(entries[i + 1]);
+    var symbol = entries[i];
+
+    // test the symbol against the stored number to perform the calculation
+    if (symbol === "+") {
+      result += nextNum;
+    } else if (symbol === "-") {
+      result -= nextNum;
+    } else if (symbol === "*") {
+      result *= nextNum;
+    } else if (symbol === "/") {
+      result /= nextNum;
     }
-    // set the result of the calculation
-    var result = Number(entries[0]);
-    for (let i = 0; i <= entries.length; i++) {
-        var nextNum = Number(entries[i+1]);
-        var symbol = entries[i];
-       
-        // test the symbol against the stored number to perform the calculation
-        if (symbol === '+') { 
-            result += nextNum
-        } else if (symbol === '-') {
-            result -= nextNum
-        } else if (symbol === '*') {
-            result *= nextNum
-        } else if (symbol === '/') {
-            result /= nextNum
-        }
-        
-        //if the number is less than zero, turn result into an absolute number, and show that it's a negative number with '-'
 
-        if (result < 0) {
-            result = Math.abs(result) + '-';
-        }
-        
+    i++;
+  }
+}
 
-        i++;
-    }
-  });
+//if the number is less than zero, turn result into an absolute number, and show that it's a negative number with '-'
+if (result < 0) {
+  result = Math.abs(result) + "-";
+} else {
+  // if any a number button is pushed after the calculation
+  entries.push(temp);
+  entries.push(val);
+  temp = "";
+}
+// push result to answer via val
+getElementById(answer).push(val(result));
+entries = [];
+temp = "";
